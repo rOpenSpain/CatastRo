21ed0c177b5a883f16074a85c04468c670862465
Ángel Delgado <angel.sherpa80@gmail.com>
2017-06-21 18:39:04 +0200

    Create test-getCOOR.R

---
+++ test/tests/test-getCOOR.R
@@ -0,0 +1,16 @@
+context("Remote procedure calls")
+
+test_that("giving all the arguments", {
+  result <- getCOOR('13077A01800039','EPSG:4230','CIUDAD REAL', 'SANTA CRUZ DE MUDELA')
+  expect_that((is.numeric(result$coord) & is.character(result$SRS)), is_true())
+})
+
+test_that("giving cadastral reference and SRS", {
+  result <- getCOOR('9872023VH5797S','EPSG:4230')
+  expect_that((is.numeric(result$coord) & is.character(result$SRS)), is_true())
+})
+
+test_that("giving only the cadastral reference", {
+  result <- getCOOR('9872023VH5797S')
+  expect_that((is.numeric(result$coord) & is.character(result$SRS)), is_true())
+})
