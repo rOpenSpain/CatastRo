eb57970cdb83b1e9d402b18deef20f7ace5cf899
Ángel Delgado <angel.sherpa80@gmail.com>
2017-06-25 20:29:12 +0200

    Update test_getCOOR.R

--- test/testthat/test_getCOOR.R
+++ test/testthat/test_getCOOR.R
@@ -14,3 +14,8 @@ test_that("giving only the cadastral reference", {
   result <- getCOOR('9872023VH5797S')
   expect_that((is.numeric(result$coord) & is.character(result$SRS)), is_true())
 })
+
+test_that("given Municipio, Provincia is needed", {
+  result <- getCOOR(RC = '13077A01800039',SRS = 'EPSG:4230', Municipio = 'SANTA CRUZ DE MUDELA')
+  expect_that(result, throws_error())
+})
