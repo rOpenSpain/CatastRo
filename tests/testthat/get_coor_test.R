6df80c985e98f4dc0621be055cbeef1e37f001cf
Ángel Delgado <angel.sherpa80@gmail.com>
2018-04-19 00:05:35 +0200

    Add files via upload

---
+++ tests/testthat/get_coor_test.R
@@ -0,0 +1,20 @@
+context("Remote procedure calls")
+
+test_that("giving all the arguments", {
+  result <- get_coor('13077A01800039','EPSG:4230','CIUDAD REAL', 'SANTA CRUZ DE MUDELA')
+  expect_that((is.numeric(result$coord) & is.character(result$SRS)), is_true())
+})
+
+test_that("giving cadastral reference and SRS", {
+  result <- get_coor('9872023VH5797S','EPSG:4230')
+  expect_that((is.numeric(result$coord) & is.character(result$SRS)), is_true())
+})
+
+test_that("giving only the cadastral reference", {
+  result <- get_coor('9872023VH5797S')
+  expect_that((is.numeric(result$coord) & is.character(result$SRS)), is_true())
+})
+
+test_that("given Municipio, Provincia is needed", {
+  expect_that(get_coor(RC = '13077A01800039',SRS = 'EPSG:4230', Municipality = 'SANTA CRUZ DE MUDELA'), throws_error())
+})
---
+++ tests/testthat/near_rc_test.R
@@ -0,0 +1,32 @@
+context("Remote procedure calls")
+
+test_that("return data.frame given SRS", {
+  result <- near_rc(lat = 40.963200, lon = -5.671420, "EPSG:4326")
+  expect_that(is.data.frame(result), is_true())
+})
+
+test_that("return data.frame without SRS", {
+  result <- near_rc(lat = 40.963200, lon = -5.671420)
+  expect_that(is.data.frame(result), is_true())
+})
+
+test_that("check fields without SRS", {
+  result <- near_rc(lat = 40.963200, lon = -5.671420)
+  expect_that((is.character(result$address) & is.character(result$RC)), is_true())
+})
+
+test_that("check fields given SRS", {
+  result <- near_rc(lat = 40.963200,lon = -5.671420,'EPSG:4230')
+  expect_that((is.character(result$address) & is.character(result$RC)), is_true())
+})
+
+test_that("if data is know return NA", {
+  result <- near_rc(lat = 99999999,lon = -999999999)
+  expect_that(nrow(result)==0, is_true())
+})
+
+
+test_that("unprecised coordinates", {
+  result <- near_rc(lat = 40.963200,lon = -5.671420, SRS = "EPSG:4326")
+  expect_that(is.data.frame(result), is_true())
+})
