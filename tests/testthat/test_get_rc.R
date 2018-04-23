04125abffecde7b1df47ad49929b8e5e4e507b7a
Ángel Delgado <angel.sherpa80@gmail.com>
2018-04-23 23:16:08 +0200

    add sleep time to test_get_rc.R

--- tests/testthat/test_get_rc.R
+++ tests/testthat/test_get_rc.R
@@ -5,28 +5,40 @@ test_that("return data.frame given SRS", {
   expect_that(is.data.frame(result), is_true())
 })
 
+Sys.sleep(10)
+
 test_that("return data.frame without SRS", {
   result <- get_rc(lat = 38.6196566583596, lon = -3.45624183836806)
   expect_that(is.data.frame(result), is_true())
 })
+
+Sys.sleep(10)
 
 test_that("check fields without SRS", {
   result <- get_rc(lat = 38.6196566583596, lon = -3.45624183836806)
   expect_that((is.character(result$address) & is.character(result$RC)), is_true())
 })
 
+Sys.sleep(10)
+
 test_that("check fields given SRS", {
   result <- get_rc(lat = 38.6196566583596,lon = -3.45624183836806,'EPSG:4230')
   expect_that((is.character(result$address) & is.character(result$RC)), is_true())
 })
+
+Sys.sleep(10)
 
 test_that("if data is know return NA", {
   result <- get_rc(lat = 99999999,lon = -999999999)
   expect_that((is.na(result$address) & is.na(result$RC)), is_true())
 })
 
+Sys.sleep(10)
+
 
 test_that("unprecised coordinates", {
   result <- get_rc(lat = 40.963200,lon = -5.671420, SRS = "EPSG:4326")
   expect_that((is.na(result$address) & is.na(result$RC)), is_true())
 })
+
+Sys.sleep(10)
