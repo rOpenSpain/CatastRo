d892c34764d9d236d7420653c8b5bfa5a5d7a9f6
Ángel Delgado <angel.sherpa80@gmail.com>
2018-04-23 23:48:43 +0200

    Update test_get_coor.R

--- tests/testthat/test_get_coor.R
+++ tests/testthat/test_get_coor.R
@@ -5,24 +5,19 @@ test_that("giving all the arguments", {
   expect_that((is.numeric(result$coord) & is.character(result$SRS)), is_true())
 })
 
-Sys.sleep(10)
 
 test_that("giving cadastral reference and SRS", {
   result <- get_coor('9872023VH5797S','EPSG:4230', sleep_time = 10)
   expect_that((is.numeric(result$coord) & is.character(result$SRS)), is_true())
 })
 
-Sys.sleep(10)
 
 test_that("giving only the cadastral reference", {
   result <- get_coor('9872023VH5797S', sleep_time = 10)
   expect_that((is.numeric(result$coord) & is.character(result$SRS)), is_true())
 })
 
-Sys.sleep(10)
 
 test_that("given Municipio, Provincia is needed", {
   expect_that(get_coor(RC = '13077A01800039',SRS = 'EPSG:4230', Municipality = 'SANTA CRUZ DE MUDELA', sleep_time = 10), throws_error())
 })
-
-Sys.sleep(10)
