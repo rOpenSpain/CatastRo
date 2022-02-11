b5c766be930ba67441f1abaf64940784b4baf845
Diego H <diego.hernangomezherrero@gmail.com>
2022-02-11 12:33:49 +0100

    Prepara para la siguiente versión (#8)
    * Package grooming
    
    * Fix issues

--- R/get_coor.R
+++ R/get_coor.R
@@ -1,79 +1,75 @@
-#'@name get_coor
-#'@aliases get_coor
+#' @name get_coor
+#' @aliases get_coor
 #'
-#'@title Interface to query Consulta_CPMRC
+#' @title Interface to query Consulta_CPMRC
 #'
-#'@description Returns the coordinate of a state in a SRS given the Province, Municipality 
-#'and Cadastral Reference of the state.
+#' @description Returns the coordinate of a state in a SRS given the Province, Municipality
+#' and Cadastral Reference of the state.
 #'
-#'@usage get_coor(RC, SRS= 'Google', Province = '',Municipality = '')
+#' @usage get_coor(RC, SRS= 'Google', Province = '',Municipality = '')
 #'
-#'@param RC Cadastral reference of the state.
-#'@param SRS The Spatial Reference System desired for the coordinates, if it is missed, the
+#' @param RC Cadastral reference of the state.
+#' @param SRS The Spatial Reference System desired for the coordinates, if it is missed, the
 #'           returned coordinates will be given in the Spatial Reference System used by
-#'           Googlemaps ('EPSG:4326'). 
-#'           
+#'           Googlemaps ('EPSG:4326').
+#'
 #'           The set of the available SRS to use are available in the vector `coordinates`
 #'           from this package. A part from that, the function accept as a SRS argument
 #'           the values "Google" and "Oficial". The first uses the SRS value used by Google
 #'           maps ('EPSG:4326') while the latter uses the oficial SRS ('EPSG:4258') in
 #'           Europe. If no SRS is given, the function by default choose the "Google" SRS.
-#'@param Province A string with the Province of the state. It is not case sensitive.
-#'@param Municipality A string with the Municipality of the state. It is not case sensitive.
+#' @param Province A string with the Province of the state. It is not case sensitive.
+#' @param Municipality A string with the Municipality of the state. It is not case sensitive.
 #'
-#'@return A list consisting on two numeric values: the coordinates (latitude, longitude) and
-#'the reference system in which they are expressed.
-#'        
-#'@references http://ovc.catastro.meh.es/ovcservweb/OVCSWLocalizacionRC/OVCCoordenadas.asmx?op=Consulta_CPMRC
+#' @return A list consisting on two numeric values: the coordinates (latitude, longitude) and
+#' the reference system in which they are expressed.
 #'
-#'@author Angel Delgado Panadero.
+#' @references http://ovc.catastro.meh.es/ovcservweb/OVCSWLocalizacionRC/OVCCoordenadas.asmx?op=Consulta_CPMRC
 #'
-#'@examples
-#'# using all the arguments
-#'coordinates <- get_coor('13077A01800039','EPSG:4230','CIUDAD REAL', 'SANTA CRUZ DE MUDELA')
-#'print(coordinates)    
-#
-#'# only the cadastral reference
-#'coordinates <- get_coor('9872023VH5797S')
-#'print(coordinates)
-#' 
+#' @author Angel Delgado Panadero.
 #'
-#'@export
-#'@import httr
-#'@import XML
+#' @examples
+#' # using all the arguments
+#' coordinates <- get_coor("13077A01800039", "EPSG:4230", "CIUDAD REAL", "SANTA CRUZ DE MUDELA")
+#' print(coordinates)
+#' #
+#' # only the cadastral reference
+#' coordinates <- get_coor("9872023VH5797S")
+#' print(coordinates)
+#' @export
 
 
 
 
 
-get_coor <- function(RC, SRS= 'Google', Province = '',Municipality = ''){
-  
+
+get_coor <- function(RC, SRS = "Google", Province = "", Municipality = "") {
+
   # SRS REPLACE
-  SRS <- ifelse(tolower(SRS) == "google", 'EPSG:4326', SRS)
-  SRS <- ifelse(tolower(SRS) == "oficial", 'EPSG:4258', SRS)
-  
+  SRS <- ifelse(tolower(SRS) == "google", "EPSG:4326", SRS)
+  SRS <- ifelse(tolower(SRS) == "oficial", "EPSG:4258", SRS)
+
   # REQUEST
-  res <- GET(url = 'http://ovc.catastro.meh.es/ovcservweb/OVCSWLocalizacionRC/OVCCoordenadas.asmx/Consulta_CPMRC',
-             query = list(Provincia=Province, Municipio=Municipality, SRS=SRS, RC=RC), 
-             ua = user_agent(paste0("CatastRo (https://github.com/rOpenSpain/CatastRo)")))
-  
+  res <- GET(
+    url = "http://ovc.catastro.meh.es/ovcservweb/OVCSWLocalizacionRC/OVCCoordenadas.asmx/Consulta_CPMRC",
+    query = list(Provincia = Province, Municipio = Municipality, SRS = SRS, RC = RC),
+    ua = user_agent(paste0("CatastRo (https://github.com/rOpenSpain/CatastRo)"))
+  )
+
   stop_for_status(res)
   res <- xmlToList(xmlParse(res))
-  
-  #TEXT MINING
-  if(is.null(res$lerr$err$des)){
 
+  # TEXT MINING
+  if (is.null(res$lerr$err$des)) {
     lon <- res$coordenadas$coord$geo$xcen
     lat <- res$coordenadas$coord$geo$ycen
     SRS <- res$coordenadas$coord$geo$srs
-    res <- list('coord' = as.numeric(c(lat,lon)), 'SRS' = SRS)
-  }
-  
-  else if(res$lerr$err$des == "LA PROVINCIA ES OBLIGATORIA"){
+    res <- list("coord" = as.numeric(c(lat, lon)), "SRS" = SRS)
+  } else if (res$lerr$err$des == "LA PROVINCIA ES OBLIGATORIA") {
     stop("LA PROVINCIA ES OBLIGATORIA")
+  } else {
+    res <- NA
   }
-  
-  else{res <- NA}
   Sys.sleep(1)
   return(res)
 }
--- R/near_rc.R
+++ R/near_rc.R
@@ -3,8 +3,8 @@
 #'
 #'@title Interface to query Consulta_RCCOOR_Distancia
 #'
-#'@description Returns the Cadastral Reference of the state as well as the address 
-#'(municipality, street and number) given its coordinates and the Spatial Reference 
+#'@description Returns the Cadastral Reference of the state as well as the address
+#'(municipality, street and number) given its coordinates and the Spatial Reference
 #'System. In case that there is not any cadastral reference in the exact point, the
 #'function returns all the cadastral references in a square 50 meters long centered
 #'in the current point.
@@ -15,14 +15,14 @@
 #'@param lon Longitude coortinate.
 #'@param SRS The Spatial Reference System used for the coordinates. There is a vector
 #'       with all the allowed SRSs in the variable from the package called coordinates.
-#'       
+#'
 #'       The set of the available SRS to use are available in the vector `coordinates`
 #'       from this package. A part from that, the function accept as a SRS argument
 #'       the values "Google" and "Oficial". The first uses the SRS value used by Google
 #'       maps ('EPSG:4326') while the latter uses the oficial SRS ('EPSG:4258') in
 #'       Europe. If no SRS is given, the function by default choose the "Google" SRS.
-#'           
-#'@return A data.frame with the the addresses of the states and the cadastral references 
+#'
+#'@return A data.frame with the the addresses of the states and the cadastral references
 #'        in the nearby area of the location given for every SRS requested.
 #'
 #'@references http://ovc.catastro.meh.es/ovcservweb/OVCSWLocalizacionRC/OVCCoordenadas.asmx?op=Consulta_RCCOOR_Distancia
@@ -31,11 +31,10 @@
 #'
 #'@examples
 #'direction <- near_rc(40.96002, -5.663408)
-#'print(direction)    
+#'print(direction)
 #'
 #'@export
-#'@import httr
-#'@import XML
+
 
 
 near_rc <- function(lat,lon,SRS="Google"){
@@ -45,56 +44,56 @@ near_rc <- function(lat,lon,SRS="Google"){
   SRS <- ifelse(tolower(SRS) == "oficial", 'EPSG:4258', SRS)
 
   # QUERY
-  res <- GET(url = 'http://ovc.catastro.meh.es/ovcservweb/OVCSWLocalizacionRC/OVCCoordenadas.asmx/Consulta_RCCOOR_Distancia', 
-             query = list(Coordenada_X=lon, Coordenada_Y=lat, SRS=SRS), 
+  res <- GET(url = 'http://ovc.catastro.meh.es/ovcservweb/OVCSWLocalizacionRC/OVCCoordenadas.asmx/Consulta_RCCOOR_Distancia',
+             query = list(Coordenada_X=lon, Coordenada_Y=lat, SRS=SRS),
              ua = user_agent("CatastRo (https://github.com/DelgadoPanadero/CatastRo)"))
-  
+
   stop_for_status(res)
   res <- xmlToList(xmlParse(res))
   distancia <- res$coordenadas_distancias$coordd$lpcd$pcd$dis
-  
+
   # TEXT MINING
   if(is.null(res$coordenadas_distancias$coordd$lpcd)){
-    
+
     # If the are not any cadastral register neither in the coordeinates nor the nearby area
     res <- data.frame(address = NA, RC = NA, SRS  = NA, stringsAsFactors = F)
-    
-  } 
-    
+
+  }
+
   else if (!is.null(res$coordenadas_distancias$coordd$lpcd) && distancia == '0'){
-    
+
     # if the coordenates are inside the space of a cadastral register
-      address <- res$coordenadas_distancias$coordd$lpcd$pcd$ldt 
+      address <- res$coordenadas_distancias$coordd$lpcd$pcd$ldt
       RC <- paste0(res$coordenadas_distancias$coordd$lpcd$pcd$pc$pc1,
                    res$coordenadas_distancias$coordd$lpcd$pcd$pc$pc2)
-      
+
       res <- data.frame(address = address, RC = RC, SRS  = SRS, stringsAsFactors = F)
-      
+
     }
-    
+
   else if (!is.null(res$coordenadas_distancias$coordd$lpcd) && distancia != '0'){
-      
+
     # The most general case, the coordinates doesn't match any cadrastral register, but there are one or more nearby
       address <- lapply(res$coordenadas_distancias$coordd$lpcd,FUN = '[[',3)
       address <- do.call(rbind,address)
       address <- as.vector(address)
-      
+
       RC <- lapply(res$coordenadas_distancias$coordd$lpcd,FUN = '[[',1)
       RC <- do.call(rbind,RC)
       RC <- as.data.frame(RC, row.names = F)
       RC <- as.vector(paste0(RC$pc1,RC$pc2))
-      
+
       res <- data.frame(address = address, RC = RC, SRS  = SRS, stringsAsFactors = F)
 
   }
-    
+
   else {
-    
+
     # If there is nothing near...
     res <- data.frame(address = NA, RC = NA, SRS  = NA, stringsAsFactors = F)
-    }    
+    }
 
   Sys.sleep(1)
-  
+
   return(res)
 }
