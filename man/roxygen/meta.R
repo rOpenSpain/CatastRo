d3d95c6394f3964098707d216da7c2f1bb1b865f
Diego H <diego.hernangomezherrero@gmail.com>
2022-02-15 18:00:43 +0100

    Update meta.R

--- man/roxygen/meta.R
+++ man/roxygen/meta.R
@@ -1,7 +1,7 @@
 list(
   rd_family_title = list(
     ATOM = "Other INSPIRE ATOM services:",
-    ATOM = "Other INSPIRE WFS services:",
+    WFS = "Other INSPIRE WFS services:",
     INSPIRE = "INSPIRE API functions:",
     OVCCoordenadas = "OVCCoordenadas API:"
   )
